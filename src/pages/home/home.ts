62f9106c22d1dbb72fa7e5d105809b862bd86651
Gayatri Belapurkar <gayatri.belapurkar5@gmail.com>
2019-02-24 19:40:21 +0530

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RestProvider } from '../../providers/rest/rest';
+
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  users: any;
+  constructor(public navCtrl: NavController, public restProvider: RestProvider) {
+    this.restProvider.getMusic()
+      .subscribe(musicList => console.log(musicList));
   }
+
+  // getUsers() {
+  //   this.restProvider.getUsers()
+  //   .then(data => {
+  //     this.users = data;
+  //     console.log(this.users);
+  //   });
+  // }
 
 }
